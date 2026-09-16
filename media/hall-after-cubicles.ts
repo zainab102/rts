06df001810ac928a4c7ab892ef573d36124730d7
Zainab <tariqzainab1190@gmail.com>
2026-09-16 17:11:26 +0500

    Add photo module batch 005

---
+++ media/hall-after-cubicles.ts
@@ -0,0 +1,6 @@
+import { p as p0 } from "./hall-after-cubicles.0";
+import { p as p1 } from "./hall-after-cubicles.1";
+import { p as p2 } from "./hall-after-cubicles.2";
+import { p as p3 } from "./hall-after-cubicles.3";
+import { p as p4 } from "./hall-after-cubicles.4";
+export const src = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4;
