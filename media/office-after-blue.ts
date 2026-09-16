63a4d6203200af6c25c8ac419ac919eea2378c6e
Zainab <tariqzainab1190@gmail.com>
2026-09-16 17:31:56 +0500

    Add photo module batch 048

---
+++ media/office-after-blue.ts
@@ -0,0 +1,6 @@
+import { p as p0 } from "./office-after-blue.0";
+import { p as p1 } from "./office-after-blue.1";
+import { p as p2 } from "./office-after-blue.2";
+import { p as p3 } from "./office-after-blue.3";
+import { p as p4 } from "./office-after-blue.4";
+export const src = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4;
