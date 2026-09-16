582fe94cba5ce85387d58b34d90b5ba2337f35d8
Zainab <tariqzainab1190@gmail.com>
2026-09-16 17:27:53 +0500

    Add photo module batch 035

---
+++ media/kitchen-after-sink.ts
@@ -0,0 +1,7 @@
+import { p as p0 } from "./kitchen-after-sink.0";
+import { p as p1 } from "./kitchen-after-sink.1";
+import { p as p2 } from "./kitchen-after-sink.2";
+import { p as p3 } from "./kitchen-after-sink.3";
+import { p as p4 } from "./kitchen-after-sink.4";
+import { p as p5 } from "./kitchen-after-sink.5";
+export const src = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4 + p5;
