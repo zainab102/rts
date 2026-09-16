cc75cb2530a9cbba869e60012c7b56668c1fc834
Zainab <tariqzainab1190@gmail.com>
2026-09-16 16:42:40 +0500

    Add photo module batch 068

---
+++ media/sales-before-windows.ts
@@ -0,0 +1,6 @@
+import { p as p0 } from "./sales-before-windows.0";
+import { p as p1 } from "./sales-before-windows.1";
+import { p as p2 } from "./sales-before-windows.2";
+import { p as p3 } from "./sales-before-windows.3";
+import { p as p4 } from "./sales-before-windows.4";
+export const src = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4;
