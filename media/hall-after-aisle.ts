442b9949f211b96a0f1c0a3d52fe211dfe3637c3
Zainab <tariqzainab1190@gmail.com>
2026-09-16 16:52:56 +0500

    Add photo module batch 002

---
+++ media/hall-after-aisle.ts
@@ -0,0 +1,6 @@
+import { p as p0 } from "./hall-after-aisle.0";
+import { p as p1 } from "./hall-after-aisle.1";
+import { p as p2 } from "./hall-after-aisle.2";
+import { p as p3 } from "./hall-after-aisle.3";
+import { p as p4 } from "./hall-after-aisle.4";
+export const src = "data:image/jpeg;base64," + p0 + p1 + p2 + p3 + p4;
